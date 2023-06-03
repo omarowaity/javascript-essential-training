bb2d3e806519cab50ddbf0b99834dcd1557822e1
omarowaity <124497911+omarowaity@users.noreply.github.com>
2023-06-03 23:14:33 +0300

    Update script.js

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,8 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+document.querySelector(".backpack__name").innerHTML = "Change Title"
+
+
+const list = document.querySelectorAll("li")
+list.forEach(item => item.innerHTML = "All lists will be the same")
