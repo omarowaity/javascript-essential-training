12329be053057566fed0d58d133e2b98d06aea34
omarowaity <124497911+omarowaity@users.noreply.github.com>
2023-06-03 23:50:35 +0300

    Update script.js

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,7 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+document.querySelector(".backpack__name").classList.add("new-class")
+document.querySelector(".backpack__name").classList.add("another-class")
+document.querySelector(".backpack__name").classList.remove("backpack__name")
+
